222bb665e877c51393bfa4139376fc0023a19e9e
Philipp Spiertz <philipp@spiertz.dev>
2024-12-01 14:05:18 +0000

    changed cypress config

--- backend/cypress.config.js
+++ backend/cypress.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'cypress'
 
 const PORT = process.env.PORT || 3000;
 
-const baseUrl = process.env.CODESPACE_NAME
-  ? `https://${process.env.CODESPACE_NAME}-${PORT}.app.github.dev`
-  : `http://localhost:${PORT}`;
+const baseUrl = `http://localhost:${PORT}`;
 
 export default defineConfig({
     e2e: {
