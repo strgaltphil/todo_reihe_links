f23a62c984df0ec1e5cf15f882c8c179dc629b40
Philipp Spiertz <philipp@spiertz.dev>
2024-12-01 16:40:59 +0100

    Feature/ci cd (#10)
    Added CI/CD

--- backend/cypress.config.js
+++ backend/cypress.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'cypress'
 
-const PORT = process.env.PORT || 3000;
-
-const baseUrl = process.env.CODESPACE_NAME
-  ? `https://${process.env.CODESPACE_NAME}-${PORT}.app.github.dev`
-  : `http://localhost:${PORT}`;
-
 export default defineConfig({
     e2e: {
-        baseUrl: baseUrl,
+        baseUrl: 'http://localhost:3000'
     },
 })
--- backend/cypress/support/e2e.js
+++ backend/cypress/support/e2e.js
@@ -2,6 +2,4 @@ beforeEach(() => {
     cy.clearCookies();
     cy.clearLocalStorage();
     cy.visit('/todo.html');
-    var button = cy.get('button').contains('Continue');
-    button ? button.click() : null;
 })
